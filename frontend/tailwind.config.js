0df68891828aa922a2d90ec0a7ae32059482c31b
Shakera Ema <shakerajannatema8685@gmail.com>
2024-06-02 03:28:53 +0600

    installation done

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx"],
+  theme: {
+    fontFamily: {
+      Roboto: ["Roboto", "sans-serif"],
+      Poppins: ['Poppins', "sans-serif"],
+    },
+  },
+  
+  plugins: [],
+}
+
