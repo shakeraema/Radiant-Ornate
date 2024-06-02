711fc103caee12d8153f71aea0b44c7fc9b72b26
Shakera Ema <shakerajannatema8685@gmail.com>
2024-06-02 23:43:26 +0600

    Loginpage added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
-import React from 'react'
-
+import React from 'react';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import {LoginPage} from './Routes.js'
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/login' elements={<LoginPage/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <p>
+hello login
+        </p>
+        </div>
+  )
+}
+
+export default Login
