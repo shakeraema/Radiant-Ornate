22fb8964a750362e78fa95e64c4abcb3b7b3870e
Tafriya Amin Habiba <121118599+tafriya713@users.noreply.github.com>
2024-06-08 11:34:42 +0600

    updated

---
+++ temp-client/src/components/Layout/Layout.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Helmet } from "react-helmet";
+
+const Layout = ({ children, title, description, keywords, author }) => {
+  return (
+    <div>
+      <helmet>
+        <meta charSet="UTF-8" />
+
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+
+        <title>{title}</title>
+      </helmet>
+
+      <Header />
+      <main style={{ minHeight: "70vh" }}>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+Layout.defaultProps={
+  title:"Ecommerce app -Shop Now",
+  description:"mern-stack-project", //change description
+  keywords:"mern,react,node,mongodb",
+  author:"tafriya",
+};
+//If want to more SEO friendly have to add thse in every pages
+
+export default Layout;
