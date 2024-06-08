6aa5e114f8cd7846a7129afb601e0961aad8f2ee
Tafriya Amin Habiba <121118599+tafriya713@users.noreply.github.com>
2024-06-08 20:34:03 +0600

    updated

--- temp-client/src/components/Layout/Layout.js
+++ temp-client/src/components/Layout/Layout.js
@@ -7,15 +7,14 @@ import { Toaster } from "react-hot-toast";
 const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
-      <helmet>
-        <meta charSet="UTF-8" />
-
-        <meta name="description" content={description} />
-        <meta name="keywords" content={keywords} />
-        <meta name="author" content={author} />
-
-        <title>{title}</title>
-      </helmet>
+<Helmet>
+  
+    <meta charSet="UTF-8" />
+    <meta name="description" content="{description}" />
+    <meta name="keywords" content="{keywords}" />
+    <meta name="author" content="{author}" />
+    <title>{'{'}title{'}'}</title>
+</Helmet>
 
       <Header />
       <main style={{ minHeight: "70vh" }}>
