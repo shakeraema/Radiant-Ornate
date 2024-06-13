1a9437a61c33ab0a3c830894f273c7465a53f9d9
Tafriya Amin Habiba <121118599+tafriya713@users.noreply.github.com>
2024-06-13 14:27:44 +0600

    file updated

--- client/src/components/Layout/AdminMenu.js
+++ client/src/components/Layout/AdminMenu.js
@@ -13,10 +13,16 @@ const AdminMenu = () => {
             Create Category
           </NavLink>
           <NavLink
-            to="/dashboard/admin/create-product"
+            to="/dashboard/admin/products"
             className="list-group-item list-group-item-action"
           >
             Create Product
+          </NavLink>
+          <NavLink
+            to="/dashboard/admin/create-product"
+            className="list-group-item list-group-item-action"
+          >
+            Products
           </NavLink>
           <NavLink
             to="/dashboard/admin/users"
