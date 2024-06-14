da2681f8ead9797d537cd390868d0a8a69d820c5
Tafriya Amin Habiba <121118599+tafriya713@users.noreply.github.com>
2024-06-14 22:19:01 +0600

    gittt

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,8 +4,13 @@ import {
   deleteProductController,
   getProductController,
   getSingleProductController,
+  productFiltersController,
   productPhotoController,
   updateProductController,
+  productCountController,
+  productListController,
+  searchProductController,
+  realtedProductController,
 } from "../controllers/productController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 import formidable from "express-formidable";
@@ -41,4 +46,18 @@ router.get("/product-photo/:pid", productPhotoController);
 //delete product
 router.delete("/delete-product/:pid", deleteProductController);
 
+//filter product
+router.post("/product-filters", productFiltersController);
+
+//product count
+router.get("/product-count", productCountController);
+//product per page
+router.get("/product-list/:page", productListController);
+
+//search product
+router.get("/search/:keyword", searchProductController);
+//similar product
+router.get("/related-product/:pid/:cid", realtedProductController);
 export default router;
+//category wise product
+router.get("/product-category/:slug", productCategoryController);
