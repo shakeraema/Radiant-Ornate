7957922b721ab559b57e764c65d9f0b982ed1143
Tafriya Amin Habiba <121118599+tafriya713@users.noreply.github.com>
2024-06-15 11:17:37 +0600

    file changes updated

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -4,10 +4,12 @@ import { useAuth } from "../../context/auth";
 import toast from "react-hot-toast";
 import SearchInput from "../Form/SearchInput";
 import useCategory from "../../hooks/useCategory";
-
+import { useCart } from "../../context/cart";
+import {} from "antd";
 const Header = () => {
   const [auth, setAuth] = useAuth();
-  const categories=useCategory()
+  const [cart] = useCart();
+  const categories = useCategory();
   const handleLogout = () => {
     setAuth({
       ...auth,
@@ -37,40 +39,38 @@ const Header = () => {
               🛒Radiant Ornate
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <SearchInput/>
+              <SearchInput />
               <li className="nav-item">
                 <NavLink to="/" className="nav-link ">
                   Home
                 </NavLink>
               </li>
               <li className="nav-item dropdown">
-              <Link
-                className="nav-link dropdown-toggle"
-                to={"/categories"}
-                data-bs-toggle="dropdown"
-              >
-                Categories
-              </Link>
-              <ul className="dropdown-menu">
-                <li>
-                  <Link className="dropdown-item" to={"/categories"}>
-                    All Categories
-                  </Link>
-                </li>
-                {categories?.map((c) => (
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to={"/categories"}
+                  data-bs-toggle="dropdown"
+                >
+                  Categories
+                </Link>
+                <ul className="dropdown-menu">
                   <li>
-                    <Link
-                      className="dropdown-item"
-                      to={`/category/${c.slug}`}
-                    >
-                      {c.name}
+                    <Link className="dropdown-item" to={"/categories"}>
+                      All Categories
                     </Link>
                   </li>
-                ))}
-              </ul>
-            </li>
-
-
+                  {categories?.map((c) => (
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to={`/category/${c.slug}`}
+                      >
+                        {c.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </li>
 
               {!auth?.user ? (
                 <>
@@ -122,9 +122,11 @@ const Header = () => {
                 </>
               )}
               <li className="nav-item">
-                <NavLink to="/cart" className="nav-link">
-                  Cart (0)
-                </NavLink>
+                <Badge count={cart?.length} showZero>
+                  <NavLink to="/cart" className="nav-link">
+                    Cart 
+                  </NavLink>
+                </Badge>
               </li>
             </ul>
           </div>
